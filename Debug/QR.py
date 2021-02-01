1cd5213bed2f54d7ac1be31a03b3eae1a53b7bde
Iliaaer <31032527+Iliaaer@users.noreply.github.com>
2021-02-01 23:18:48 +0400

    Update QR.py

--- Debug/QR.py
+++ Debug/QR.py
@@ -1,5 +1,5 @@
 from pyzbar import pyzbar
-
+from numpy import array
 def detectQR(img):
     barcod = pyzbar.decode(img)
     
@@ -8,5 +8,5 @@ def detectQR(img):
     b_data = barcod.data.encode("utf-8")
     (x, y, w, h) = barcode.rect
 
-    return [b_data, (x + w/2, y + h/2), [(x, y), (x + w, y), (x + w, y + h), (x, y + h)]]
+    return [b_data, (x + w/2, y + h/2), array([array([x, y]), array([x + w, y]), array([x + w, y + h]), array([x, y + h])])]
 
