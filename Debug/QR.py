daa322b400cfb478cdcaa87e2dece11185536704
Iliaaer <31032527+Iliaaer@users.noreply.github.com>
2021-02-01 22:56:44 +0400

    Add files via upload

---
+++ Debug/QR.py
@@ -0,0 +1,12 @@
+from pyzbar import pyzbar
+
+def detectQR(img):
+    barcod = pyzbar.decode(img)
+    
+    barcod = barcod[0]
+
+    b_data = barcod.data.encode("utf-8")
+    (x, y, w, h) = barcode.rect
+
+    return [b_data, (x, y), (x + w, y), (x + w, y + h), (x, y + h), (x + w/2, y + h/2)]
+
