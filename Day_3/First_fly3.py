808cd60c03018993cf824fb0b34beb3f19e089cf
petayyyy <47917455+petayyyy@users.noreply.github.com>
2021-02-04 14:50:17 +0300

    Update First_fly3.py

--- Day_3/First_fly3.py
+++ Day_3/First_fly3.py
@@ -6,7 +6,7 @@ import time
 import roslib
 import sys
 import rospy
-from ConfigColorDetecting1 import ColorDetecting
+from ConfigColorDetecting2 import ColorDetecting
 from led import led
 from sensor_msgs.msg import Image
 import threading
